a8a5df96cb26e1efbe9a2ef35667b1754bc72514
Luis Valadez <valopezla2sc@gmail.com>
2022-10-12 09:53:06 -0500

    set up US in new branch

---
+++ src/test/java/com/vytrack/test/US49_AccessMainModules.java
@@ -0,0 +1,23 @@
+package com.vytrack.test;
+
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class US49_AccessMainModules {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+
+
+}
