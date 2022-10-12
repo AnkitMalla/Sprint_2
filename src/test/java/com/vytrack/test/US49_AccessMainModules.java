ee0fe88fee8c648420ba5a1571efb1d7b49758b7
Luis Valadez <valopezla2sc@gmail.com>
2022-10-12 10:04:12 -0500

    started test

--- src/test/java/com/vytrack/test/US49_AccessMainModules.java
+++ src/test/java/com/vytrack/test/US49_AccessMainModules.java
@@ -1,10 +1,13 @@
 package com.vytrack.test;
 
 import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.security.Key;
 import java.util.concurrent.TimeUnit;
 
 public class US49_AccessMainModules {
@@ -15,11 +18,14 @@ public class US49_AccessMainModules {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://qa2.vytrack.com/user/login");
     }
 
     //log in as manager
     @Test
     public void login_manager_credentials(){
+        WebElement userName1 = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+
 
     }
 
